dc3f5036d6916794c27d3529564e70ba12ecf8e2
wangyi <haveaballmyball@hotmail.com>
2019-03-26 20:50:02 +0800

    no message

---
+++ src/utils/message.js
@@ -0,0 +1,15 @@
+import { Message,MessageBox } from "element-ui";
+export class MessageBounced {
+  constructor(msgText,msgType,mainContent,fn) {
+    this.msgText = msgText;
+    this.msgType = msgType;
+    this.mainContent = mainContent;
+    this.fn = fn;
+  }
+  messageWindow(){
+    Message({message:this.msgText,type:this.msgType})
+  }
+  confirmWindow(){ 
+    MessageBox.confirm(this.mainContent,this.msgText,{callback:this.fn})
+  }
+}
