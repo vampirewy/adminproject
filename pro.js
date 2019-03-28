6e741f826fee9f1d406390f62d816f2ef07d0ad8
wangyi <haveaballmyball@hotmail.com>
2019-03-28 17:49:16 +0800

    添加pro自动上传ftp脚本

---
+++ pro.js
@@ -0,0 +1,13 @@
+const client = require('scp2');
+client.scp('dist/', {
+  host: '120.77.39.79',
+  username: 'ppy',
+  password: 'Ppy@079@#,',
+  path: '/web/admin/op'
+}, function (err) {
+  if (err) {
+    console.log(err)
+  } else {
+    console.log('文件上传完毕!\n')
+  }
+});
