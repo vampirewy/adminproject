8053fb1255c7879a6479a5f8850b3218cb3fb298
wangyi <haveaballmyball@hotmail.com>
2019-04-03 15:43:04 +0800

    no message

--- src/components/pageBar.vue
+++ src/components/pageBar.vue
@@ -23,7 +23,7 @@ export default {
     pageNum: Number,
     totalPage: Number,
     totalCount: Number,
-    traId: Number,
+    traId: String,
     statusLists: String,
     name:String //导购名称或专题名称
   },
