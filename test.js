e1f6bc50b1047990a95c6348aff83181ed73baf1
wangyi <haveaballmyball@hotmail.com>
2019-04-11 20:28:02 +0800

    dv2迁移至test

---
+++ test.js
@@ -0,0 +1,13 @@
+const client = require('scp2');
+client.scp('dist/', {
+  host: '172.16.31.47',
+  username: 'ppy',
+  password: 'pg@app',
+  path: '/ppy/web/admin/op'
+}, function (err) {
+  if (err) {
+    console.log(err)
+  } else {
+    console.log('文件上传完毕!\n')
+  }
+});
