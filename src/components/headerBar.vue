006a656bd8272ea169c751ba993a0b08c91573ed
wangyi <haveaballmyball@hotmail.com>
2019-04-12 14:28:28 +0800

    no message

--- src/components/headerBar.vue
+++ src/components/headerBar.vue
@@ -1,16 +1,12 @@
 <template>
-  <div class="header_bar">
-    <header class="header">
-      <div class="left right">
-        <el-checkbox :indeterminate="isIndeterminate" v-model="checkAll" @change="selectAll">全选</el-checkbox>
-        <el-checkbox-group v-model="statusList" @change="seletItem">
-          <el-checkbox
-            v-for="(item,index) in checkBoxLists"
-            :label="item.name"
-            :key="index"
-          >{{item.name}}</el-checkbox>
-        </el-checkbox-group>
-        <!-- <el-checkbox-group v-model="statusList">
+<div class="header_bar">
+  <header class="header">
+    <div class="left right">
+      <el-checkbox :indeterminate="isIndeterminate" v-model="checkAll" @change="selectAll">全选</el-checkbox>
+      <el-checkbox-group v-model="statusList" @change="seletItem">
+        <el-checkbox v-for="(item,index) in checkBoxLists" :label="item.name" :key="index">{{item.name}}</el-checkbox>
+      </el-checkbox-group>
+      <!-- <el-checkbox-group v-model="statusList">
           <el-checkbox
             @change="changeStatus(item)"
             v-for="(item,index) in checkBoxLists"
@@ -18,44 +14,40 @@
             :key="index"
           >{{item.name}}</el-checkbox>
         </el-checkbox-group>-->
-      </div>
-      <div class="middle">
-        <el-select v-model="value" @blur="changeArea()">
-          <el-option
-            v-for="item in allAreaName"
-            :key="item.traName"
-            :label="item.traName"
-            :value="item.traId"
-          ></el-option>
-        </el-select>
-      </div>
-      <!-- 导购列表、专题列表 -->
-      <div class="right" v-if="showArea">
-        <el-input placeholder="请输入名称" prefix-icon="el-icon-search" v-model="inputText" clearable></el-input>
-        <el-button type="primary" class="search" @click="search()">搜索</el-button>
-        <el-button type="danger" @click="add()">添加</el-button>
-      </div>
-      <!-- 弹窗列表 -->
-      <div class="right" v-if="!showArea">
-        <el-autocomplete
-          v-model="popName"
-          :fetch-suggestions="querySearchAsync"
-          placeholder="请输入弹窗名称"
-          @select="selecthPopName"
-        ></el-autocomplete>
-        <el-button type="primary" class="search m_r_10" @click="searchPop()">搜索</el-button>
-        <router-link to="/addpop">
-          <el-button type="danger">添加</el-button>
-        </router-link>
-      </div>
-    </header>
-  </div>
+    </div>
+    <div class="middle">
+      <el-select v-model="value" @blur="changeArea()">
+        <el-option v-for="item in allAreaName" :key="item.traName" :label="item.traName" :value="item.traId"></el-option>
+      </el-select>
+    </div>
+    <!-- 导购列表、专题列表 -->
+    <div class="right" v-if="showArea">
+      <el-input placeholder="请输入名称" prefix-icon="el-icon-search" v-model="inputText" clearable></el-input>
+      <el-button type="primary" class="search" @click="search()">搜索</el-button>
+      <el-button type="danger" @click="add()">添加</el-button>
+    </div>
+    <!-- 弹窗列表 -->
+    <div class="right" v-if="!showArea">
+      <el-autocomplete v-model="popName" :fetch-suggestions="querySearchAsync" placeholder="请输入弹窗名称" @select="selecthPopName"></el-autocomplete>
+      <el-button type="primary" class="search m_r_10" @click="searchPop()">搜索</el-button>
+      <router-link to="/addpop">
+        <el-button type="danger">添加</el-button>
+      </router-link>
+    </div>
+  </header>
+</div>
 </template>
 
 <script>
-import { allArea } from "@/api/headerBar";
-import { checkSpecial } from "@/api/shoppingGuide";
-import { popList } from "@/api/popManager";
+import {
+  allArea
+} from "@/api/headerBar";
+import {
+  checkSpecial
+} from "@/api/shoppingGuide";
+import {
+  popList
+} from "@/api/popManager";
 export default {
   name: "headerBar",
   props: {
@@ -68,11 +60,26 @@ export default {
       statusList: [], //选择的状态值  0-未生效 1-生效中 2-已结束 3-已停用 4-已删除
       checkBoxLists: [
         // { name: "全部状态", status: `` },
-        { name: "未生效", status: 0 },
-        { name: "生效中", status: 1 },
-        { name: "已结束", status: 2 },
-        { name: "已停用", status: 3 },
-        { name: "已删除", status: 4 }
+        {
+          name: "未生效",
+          status: 0
+        },
+        {
+          name: "生效中",
+          status: 1
+        },
+        {
+          name: "已结束",
+          status: 2
+        },
+        {
+          name: "已停用",
+          status: 3
+        },
+        {
+          name: "已删除",
+          status: 4
+        }
       ],
       allAreaName: [], //商圈
       inputText: ``, //除弹窗输入框外的输入框
@@ -85,7 +92,9 @@ export default {
   methods: {
     selectAll(val) {
       this.isIndeterminate = false;
-      val ? this.checkBoxLists.forEach(el => {this.statusList.push(el.name);}) : (this.statusList = []);
+      val ? this.checkBoxLists.forEach(el => {
+        this.statusList.push(el.name);
+      }) : (this.statusList = []);
     },
     seletItem(value) {
       let checkedCount = value.length;
@@ -104,7 +113,7 @@ export default {
         this.allAreaName = res.data.body;
       }
     },
-    /** 
+    /**
      * @statusList Array  选中的状态值
      * @checkBoxLists Array  状态栏
      * @value  选中的商圈ID号
@@ -126,17 +135,17 @@ export default {
       }
       this.$emit(
         "statusAreaName",
-        this.value,  //商圈ID号
-        statusList,   //状态值
-        this.inputText,  //导购、专题输入框
-        this.num  //页面1
+        this.value, //商圈ID号
+        statusList, //状态值
+        this.inputText, //导购、专题输入框
+        this.num //页面1
       );
     },
     add() {
       console.log(this.$route);
-      this.$route.name === `shoppingGuide`
-        ? this.$router.push(`/addshopping`)
-        : this.$router.push(`/specialinfor`);
+      this.$route.name === `shoppingGuide` ?
+        this.$router.push(`/addshopping`) :
+        this.$router.push(`/specialinfor`);
     },
     // changeStatus(item) {
     //   /**
@@ -160,12 +169,14 @@ export default {
       // );
     },
     searchPopName(popName, fn) {
-      let params = {windowName: popName};
+      let params = {
+        windowName: popName
+      };
       popList(params).then(
         res => {
           console.log(res.data);
           if (res.data.statusCode === 2000) {
-            res.data.body.pageData.forEach(el=>{
+            res.data.body.pageData.forEach(el => {
               el.value = el.windowName;
             });
             this.searchLists = res.data.body.pageData;
@@ -196,7 +207,7 @@ export default {
       }
       console.log(statusList);
       //状态选择 弹窗名称
-      this.$emit("searchPopName", this.value ,statusList, this.popName,this.num);
+      this.$emit("searchPopName", this.value, statusList, this.popName, this.num);
     }
   },
   created() {
@@ -208,25 +219,25 @@ export default {
 
 <style scoped lang="less">
 .flex() {
-  display: flex;
-  align-items: center;
+    display: flex;
+    align-items: center;
 }
 .header_bar {
-  margin-bottom: 20px;
+    margin-bottom: 20px;
 }
 .header,
 .right {
-  .flex();
+    .flex();
 }
-.right,
+.left,
 .middle,
-.left {
-  margin-right: 10px;
+.right {
+    margin-right: 10px;
 }
 .search {
-  margin-left: 10px;
+    margin-left: 10px;
 }
 .m_r_10 {
-  margin-right: 10px;
+    margin-right: 10px;
 }
 </style>
