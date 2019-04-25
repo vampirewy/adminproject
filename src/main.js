a1a15e40af1931354a8c86f51acba15433c957ee
wangyi <haveaballmyball@hotmail.com>
2019-04-25 15:39:25 +0800

    no message

--- src/main.js
+++ src/main.js
@@ -2,27 +2,28 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import './plugins/element.js';
-import 'normalize.css';
-import './less/global.css';
+import "./plugins/element.js";
+import "normalize.css";
+import "./less/global.css";
 import img from "./img/demo1.png";
 
 Vue.config.productionTip = false;
 
 function getCookie(name) {
-  let arr, reg = new RegExp("(^| )" + name + "=([^;]*)(;|$)");
-  if (arr = document.cookie.match(reg)) {
+  let arr,
+    reg = new RegExp("(^| )" + name + "=([^;]*)(;|$)");
+  if ((arr = document.cookie.match(reg))) {
     return unescape(arr[2]);
   } else {
     return null;
   }
-};
+}
 let sessionId = getCookie(`sessionId`);
 let cityCode = getCookie(`cityCode`);
 
 new Vue({
   router,
-  store, 
+  store,
   render: h => h(App),
   created() {
     localStorage.setItem(`sessionId`, sessionId);
@@ -41,10 +42,19 @@ function showspeed() {
   let filesize = 37;
   let endTime = new Date();
   let speed = Math.round(filesize * 1000) / (endTime - startTime);
-  let scope = (speed > 0 && speed <= 50) ? 0 : (speed > 50 && speed <= 100) ? 1 : (speed >= 100 && speed < 200) ? 2 : (speed >= 200 && speed < 300) ? 3 : 4;
+  let scope =
+    speed > 0 && speed <= 50
+      ? 0
+      : speed > 50 && speed <= 100
+      ? 1
+      : speed >= 100 && speed < 200
+      ? 2
+      : speed >= 200 && speed < 300
+      ? 3
+      : 4;
   localStorage.setItem(`speed`, arr[scope]);
   console.log(`调了`);
-};
-testImg.onload = function () {
-  showspeed();
 }
+testImg.onload = function() {
+  showspeed();
+};
